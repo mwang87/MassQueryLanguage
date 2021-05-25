faba21380bbe1bb922c20c4706c688a37c26c2ab
Mingxun Wang <mwang87@gmail.com>
2021-05-25 11:13:27 -0700

    adding testing

--- test.py
+++ test.py
@@ -74,6 +74,7 @@ def test_diphen_combo():
 def test_variable():
     # This finds the sum of the MS1 of the MS2 spectrum with 
     query = "QUERY scansum(MS1DATA) WHERE MS1MZ=X AND MS2PREC=X AND MS2PROD=160.5"
+    print(json.dumps(msql_parser.parse_msql(query), indent=4))
     results_df = msql_engine.process_query(query, "test/GNPS00002_A3_p.mzML")
     print(results_df)
 
@@ -112,8 +113,8 @@ def main():
     #test_query()
     #test_xic()
     #test_subquery()
-    #test_variable()
-    test_filter()
+    test_variable()
+    #test_filter()
 
 if __name__ == "__main__":
     main()
