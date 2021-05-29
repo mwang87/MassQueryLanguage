6a2774bfe564c94a3e8a73155d82e928c920b692
Mingxun Wang <mwang87@gmail.com>
2021-05-29 09:30:37 -0700

    setting proper input

--- proteosafe/msql/tools/msql/proteosafe_query.py
+++ proteosafe/msql/tools/msql/proteosafe_query.py
@@ -5,7 +5,10 @@ import os
 import argparse
 import sys
 
-def main():    
+def main():
+    input_folder = sys.argv[1]
+    workflow_params = sys.argv[2]
+    output_folder = sys.argv[3]
     print("XXX")
 
 if __name__ == "__main__":
