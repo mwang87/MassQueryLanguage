ac8ed53fdf9bd46e2704fa3d07337c5caf33371d
Mingxun Wang <mwang87@gmail.com>
2021-06-28 15:26:16 -0700

    putting everything in one environment

--- proteosafe/msql-nf/tools/msql-nf/nextflow_wrapper.py
+++ proteosafe/msql-nf/tools/msql-nf/nextflow_wrapper.py
@@ -11,14 +11,13 @@ def main():
     parser.add_argument('workflow_params', help='workflow_params, from proteosafe')
     parser.add_argument('nextflow_script', help='nextflow_script to actually run')
     parser.add_argument('conda_activate', help='conda_activate, this is the path to the activate command in the main conda installation')
-    parser.add_argument('nextflow_conda_environment', help='nextflow_conda_environment, this likely should be nextflow')
-    parser.add_argument('--conda', default=None, help='conda path')
+    parser.add_argument('nextflow_conda_environment', help='nextflow_conda_environment, this likely should be wherever all your dependencies and nextflow are installed, e.g. nextflow or msql2')
     parser.add_argument('--parametermapping', action='append', help='mapping of current workflow parameters to new parameters in the format: <old parameter>:<new parameter>')
     parser.add_argument('--newparameters', action='append', help='parameter key: <param name>:<parameter value>')
 
     args = parser.parse_args()
 
-    cmd = "source {} {} nextflow run {}".format(args.conda_activate, args.nextflow_conda_environment,
+    cmd = "source {} {} && nextflow run {}".format(args.conda_activate, args.nextflow_conda_environment,
                         args.nextflow_script)
     for parameter in args.newparameters:
         print(parameter)
@@ -31,9 +30,10 @@ def main():
         old_param = parameter.split(":")[0]
         cmd += ' --{} "{}"'.format(new_param, params_obj[old_param][0])
 
-    if args.conda is not None:
-        cmd += " -with-conda {}".format(args.conda)
+    #if args.conda is not None:
+    #    cmd += " -with-conda {}".format(args.conda)
 
+    print(cmd)
     os.system(cmd)
 
 
