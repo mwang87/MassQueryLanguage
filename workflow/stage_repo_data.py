7a654635a41f39a4bfbdacea9e09f99126384403
Mingxun Wang <mwang87@gmail.com>
2021-06-30 22:52:42 -0700

    staging data

---
+++ workflow/stage_repo_data.py
@@ -0,0 +1,41 @@
+import json
+import os
+import argparse
+import shutil
+import glob
+import sys
+import requests
+import pandas as pd
+import ming_proteosafe_library
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser(description="Staging public data for Nextflow")
+    parser.add_argument('output_staged', help='output_staged')
+
+    args = parser.parse_args()
+
+    # Getting all GNPS Datasets
+    all_datasets = requests.get("https://massive.ucsd.edu/ProteoSAFe/datasets_json.jsp").json()["datasets"]
+    gnps_datasets = [dataset for dataset in all_datasets if "GNPS" in dataset["title"].upper()]
+
+    print("NUMBER GNPS DATASETS", len(gnps_datasets))
+
+    for dataset in gnps_datasets:
+        print("linking", dataset["dataset"])
+        link_dataset(dataset["dataset"], args.output_staged)
+
+def link_dataset(accession, output_folder):
+    source_ccms_peak = os.path.join("/data/massive-ro/{}/ccms_peak".format(accession))
+
+    if os.path.exists(source_ccms_peak):
+        target_location = os.path.join("repo_data", accession, "ccms_peak")
+
+        output_path = Path(target_location)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        output_path.symlink_to(source_ccms_peak)
+
+
+
+if __name__ == "__main__":
+    main()
