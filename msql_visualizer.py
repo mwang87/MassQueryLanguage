b588289ed3ed48520c263088d302b8575f7fab4f
Mingxun Wang <mwang87@gmail.com>
2021-07-04 16:05:49 -0700

    rudimenatry drawing

--- msql_visualizer.py
+++ msql_visualizer.py
@@ -2,7 +2,7 @@ import msql_parser
 import msql_engine
 import plotly.graph_objects as go
 
-def visualize_query(query, variable_x=500, variable_y=1):
+def visualize_query(query, variable_x=500, variable_y=1, precursor_mz=800):
     parsed_query = msql_parser.parse_msql(query)
 
     # Cleaning up variables
@@ -22,15 +22,35 @@ def visualize_query(query, variable_x=500, variable_y=1):
                 line=dict(
                     color="RoyalBlue",
                     width=2,
-                ),
-                fillcolor="LightSkyBlue",
+                )
             )
 
-            print(condition)
+        if condition["type"] == "ms2neutrallosscondition" and condition["conditiontype"] == "where":
+            mz = condition["value"][0]
+            mz_tol = msql_engine._get_mz_tolerance(condition.get("qualifiers", None), mz)
+            nl_min = mz - mz_tol
+            nl_max = mz + mz_tol
+
+            mz_min = precursor_mz - nl_max
+            mz_max = precursor_mz - nl_min
+
+            fig.add_shape(type="rect",
+                x0=mz_min, y0=0, x1=mz_max, y1=1,
+                line=dict(
+                    color="Red",
+                    width=2,
+                )
+            )
+
 
     # Set axes properties
     fig.update_xaxes(range=[0, 1000], showgrid=False)
     fig.update_yaxes(range=[0, 1])
 
+    fig.update_layout(
+        title="Query Visualization, Precursor m/z {}".format(precursor_mz),
+        xaxis_title="m/z",
+        yaxis_title="intensity",
+    )
 
     return fig
