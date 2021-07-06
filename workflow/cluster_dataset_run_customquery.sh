3fcbbd98cab2c82a7cc9e201e678d3b60dd1db33
Mingxun Wang <mwang87@gmail.com>
2021-07-05 23:24:59 -0700

    custom query with output folder

--- workflow/cluster_dataset_run_customquery.sh
+++ workflow/cluster_dataset_run_customquery.sh
@@ -3,4 +3,5 @@ nextflow run workflow.nf -c cluster.config \
         --publishdir="./nf_$1" \
 		--parallel_files="YES" \
         --query="$2" \
+        --publishdir="$3" \
         -with-trace -resume
