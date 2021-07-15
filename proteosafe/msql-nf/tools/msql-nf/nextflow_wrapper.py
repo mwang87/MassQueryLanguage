06eb1954bc315cacf763240838dc846aec01bc30
Mingxun Wang <mwang87@gmail.com>
2021-07-15 11:00:47 -0700

    adding trace reports

--- proteosafe/msql-nf/tools/msql-nf/nextflow_wrapper.py
+++ proteosafe/msql-nf/tools/msql-nf/nextflow_wrapper.py
@@ -14,11 +14,18 @@ def main():
     parser.add_argument('nextflow_conda_environment', help='nextflow_conda_environment, this likely should be wherever all your dependencies and nextflow are installed, e.g. nextflow or msql2')
     parser.add_argument('--parametermapping', action='append', help='mapping of current workflow parameters to new parameters in the format: <old parameter>:<new parameter>')
     parser.add_argument('--newparameters', action='append', help='parameter key: <param name>:<parameter value>')
+    parser.add_argument('--runcluster', default='NO', help='Tries to run this on the cluster, values are NO and YES')
+    parser.add_argument('--clusterconfig', default=None, help='Path to configuration file')
+    parser.add_argument('--user', default=None, help='username running the task')
 
     args = parser.parse_args()
 
-    cmd = "source {} {} && nextflow run {}".format(args.conda_activate, args.nextflow_conda_environment,
-                        args.nextflow_script)
+    if args.runcluster == "YES" and args.user in ["mwang87"]:
+        cmd = "source {} {} && nextflow run {} -c {} -with-trace -with-dag -with-report report.html".format(args.conda_activate, args.nextflow_conda_environment,
+                            args.nextflow_script, args.clusterconfig)
+    else:
+        cmd = "source {} {} && nextflow run {} -with-trace -with-dag -with-report report.html".format(args.conda_activate, args.nextflow_conda_environment,
+                            args.nextflow_script)
     for parameter in args.newparameters:
         print(parameter)
         cmd += ' --{} "{}"'.format(parameter.split(":")[0], parameter.split(":")[1].replace("\n", ""))
