664fe1a7ba6efb5dccfd1ff35593214cc53a2d94
Mingxun Wang <mwang87@gmail.com>
2021-07-18 12:10:38 -0700

    downloading files

--- test/get_data.sh
+++ test/get_data.sh
@@ -1,9 +1,9 @@
-wget --output-document=GNPS00002_A3_p.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000084494/ccms_peak/raw/GNPS00002_A3_p.mzML
-wget --output-document=QC_0.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000085852/ccms_peak/QC_raw/QC_0.mzML
-wget --output-document=bld_plt1_07_120_1.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000085944/ccms_peak/raw_data/bld_plt1_07_120_1.mzML
-wget --output-document=NS_1x_test.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000087352/updates/2021-05-03_allegraaron_95d2215b/peak/NS_1x_test.mzML
-wget --output-document=JB_182_2_fe.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000084289/ccms_peak/JB_182_2_fe.mzML
-wget --output-document=S_N2_neutral_Zn.mzML https://gnps-external.ucsd.edu/massiveftpproxy?ftppath=ftp://massive.ucsd.edu/MSV000083387/updates/2019-11-12_allegraaron_e893cb7e/peak/S_N2_neutral_Zn.mzML
+wget --output-document=GNPS00002_A3_p.mzML ftp://massive.ucsd.edu/MSV000084494/ccms_peak/raw/GNPS00002_A3_p.mzML
+wget --output-document=QC_0.mzML ftp://massive.ucsd.edu/MSV000085852/ccms_peak/QC_raw/QC_0.mzML
+wget --output-document=bld_plt1_07_120_1.mzML ftp://massive.ucsd.edu/MSV000085944/ccms_peak/raw_data/bld_plt1_07_120_1.mzML
+wget --output-document=NS_1x_test.mzML ftp://massive.ucsd.edu/MSV000087352/updates/2021-05-03_allegraaron_95d2215b/peak/NS_1x_test.mzML
+wget --output-document=JB_182_2_fe.mzML ftp://massive.ucsd.edu/MSV000084289/ccms_peak/JB_182_2_fe.mzML
+wget --output-document=S_N2_neutral_Zn.mzML ftp://massive.ucsd.edu/MSV000083387/updates/2019-11-12_allegraaron_e893cb7e/peak/S_N2_neutral_Zn.mzML
 wget --output-document=gnps.json https://gnps-external.ucsd.edu/gnpslibrary/ALL_GNPS.json
 wget --output-document=gnps-library.json https://gnps-external.ucsd.edu/gnpslibrary/GNPS-LIBRARY.json
 wget --output-document=specs_ms.mgf "http://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?task=5ecfcf81cb3c471698995b194d8246a0&block=main&file=spectra/specs_ms.mgf"
