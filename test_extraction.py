3e00e4306332ac2a44113ee1c5d9c0938ca9108c
Mingxun Wang <mwang87@gmail.com>
2021-08-13 12:59:26 -0700

    adding test

--- test_extraction.py
+++ test_extraction.py
@@ -32,10 +32,24 @@ def test_extract_mzXML():
     merged_summary_df = msql_extract._extract_spectra(results_df, "test", output_json_filename="test.json")
     assert(len(merged_summary_df) == 5)
     
+def test_extract_MGF():
+    query = "QUERY scaninfo(MS2DATA)"
+    results_df = msql_engine.process_query(query, "test/specs_ms.mgf")
+    print(results_df)
+
+    assert(len(results_df) > 1)
+    results_df = results_df[:5]
+
+    results_df["filename"] = "specs_ms.mgf"
+
+    print("Extracting", len(results_df))
+    merged_summary_df = msql_extract._extract_spectra(results_df, "test", output_json_filename="test.json")
+    assert(len(merged_summary_df) == 5)
 
 def main():
     #test_extract_mzML()
-    test_extract_mzXML()
+    #test_extract_mzXML()
+    test_extract_MGF()
 
 if __name__ == "__main__":
     main()
