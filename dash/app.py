5b12d0df867b323e4604830084562b411952264e
Mingxun Wang <mwang87@gmail.com>
2021-08-19 16:58:29 -0700

    updating languages

--- dash/app.py
+++ dash/app.py
@@ -363,7 +363,7 @@ def _render_parse(query):
     output_list = [html.Hr(), parse_markdown]
 
     # Creating written description that is translated
-    languages = ["russian", "korean", "chinese", "french", "german", "spanish", "portuguese", "english"]
+    languages = ["arabic", "russian", "korean", "chinese", "french", "german", "spanish", "portuguese", "english"]
 
     for language in languages:
         try:
