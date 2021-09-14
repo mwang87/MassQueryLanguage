b66a1adacc69d461794cf3109781754cc126c859
Mingxun Wang <mwang87@gmail.com>
2021-09-14 13:38:24 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="massql",
-    version="0.0.8",
+    version="0.0.9",
     author="Mingxun Wang",
     author_email="mwang87@gmail.com",
     description="Mass spectrometry query language python implementation",
