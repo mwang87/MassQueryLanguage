4fc5889670be6a33722a4c282422c76451d9f192
Mingxun Wang <mwang87@gmail.com>
2025-12-09 18:23:55 -0800

    updating download

--- tests/get_data.sh
+++ tests/get_data.sh
@@ -8,7 +8,7 @@ wget --no-verbose --output-document=bld_plt1_07_120_1.mzML "https://massive.ucsd
 wget --no-verbose --output-document=NS_1x_test.mzML "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?forceDownload=true&file=f.MSV000087352/updates/2021-05-03_allegraaron_95d2215b/peak/NS_1x_test.mzML"
 wget --no-verbose --output-document=JB_182_2_fe.mzML "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?forceDownload=true&file=f.MSV000084289/ccms_peak/JB_182_2_fe.mzML"
 wget --no-verbose --output-document=S_N2_neutral_Zn.mzML "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?forceDownload=true&file=f.MSV000083387/updates/2019-11-12_allegraaron_e893cb7e/peak/S_N2_neutral_Zn.mzML"
-wget --no-verbose --output-document=gnps-library.json "https://gnps-external.ucsd.edu/gnpslibrary/GNPS-LIBRARY.json"
+wget --no-verbose --output-document=gnps-library.json "https://external.gnps2.org/gnpslibrary/GNPS-LIBRARY.json"
 wget --no-verbose --output-document=specs_ms.mgf "http://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?task=5ecfcf81cb3c471698995b194d8246a0&block=main&file=spectra/specs_ms.mgf"
 wget --no-verbose --output-document=1810E-II.mzML "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?file=f.MSV000084691/ccms_peak/1810E-II.mzML&forceDownload=true"
 wget --no-verbose --output-document=KoLRI_24666_Cent.mzML "http://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?task=5fc1673650f446c1b803c6391d15d0e7&block=main&file=kbkang/Lichen_library/KoLRI_24666_Cent.mzML"
