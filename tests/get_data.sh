0d5918518bfea898a8ab4806909cf0e998c11425
mwang87 <mwang87@gmail.com>
2026-03-16 19:39:57 -0700

    updating download

--- tests/get_data.sh
+++ tests/get_data.sh
@@ -9,7 +9,7 @@ wget --no-verbose --output-document=NS_1x_test.mzML "https://massiveproxy.gnps2.
 wget --no-verbose --output-document=JB_182_2_fe.mzML "https://massiveproxy.gnps2.org/massiveproxy/MSV000084289/ccms_peak/JB_182_2_fe.mzML"
 wget --no-verbose --output-document=S_N2_neutral_Zn.mzML "https://massiveproxy.gnps2.org/massiveproxy/MSV000083387/updates/2019-11-12_allegraaron_e893cb7e/peak/S_N2_neutral_Zn.mzML"
 wget --no-verbose --output-document=gnps-library.json "https://external.gnps2.org/gnpslibrary/GNPS-LIBRARY.json"
-wget --no-verbose --output-document=specs_ms.mgf "http://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?task=5ecfcf81cb3c471698995b194d8246a0&block=main&file=spectra/specs_ms.mgf"
+wget --no-verbose --tries=3 --waitretry=5 --output-document=specs_ms.mgf "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?task=5ecfcf81cb3c471698995b194d8246a0&block=main&file=spectra/specs_ms.mgf"
 wget --no-verbose --output-document=1810E-II.mzML "https://massiveproxy.gnps2.org/massiveproxy/MSV000084691/ccms_peak/1810E-II.mzML"
 wget --no-verbose --output-document=T04251505.mzXML "https://massiveproxy.gnps2.org/massiveproxy/MSV000082797/ccms_peak/raw/MTBLS368/T04251505.mzXML"
 wget --no-verbose --output-document=isa_9_fe.mzML "https://massiveproxy.gnps2.org/massiveproxy/MSV000084030/ccms_peak/isa_9_fe.mzML"
